26b2b0c9b80c29e152308acd6e1260f5099fce7b
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-06-19 00:32:26 +0200

    Add initial view for WeSplit, quickly calculate the amount to split and present it

--- hundredDaysSwiftUI.WeSplit/ContentView.swift
+++ hundredDaysSwiftUI.WeSplit/ContentView.swift
@@ -4,14 +4,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount: Float = 0.0
+    @State private var numberOfPeople: Int = 2
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Total Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        .keyboardType(.decimalPad)
+
+
+                    Picker("Amount of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) { amount in
+                            Text("\(amount) people")
+                        }
+                    }
+                }.padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+
+                Section {
+                    Text("The split amount is \(amount / Float(numberOfPeople + 2))")
+                }
+            }.navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
