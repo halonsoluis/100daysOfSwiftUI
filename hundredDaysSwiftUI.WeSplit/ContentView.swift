1dc81c93c81d635757f9101b6dde2e022f1dc710
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-06-19 00:06:13 +0200

    Initial Commit

---
+++ hundredDaysSwiftUI.WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+// Created for hundredDaysSwiftUI.WeSplit in 2023
+// Using Swift 5.0
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hundredDaysSwiftUI.WeSplit/hundredDaysSwiftUI_WeSplitApp.swift
@@ -0,0 +1,13 @@
+// Created for hundredDaysSwiftUI.WeSplit in 2023
+// Using Swift 5.0
+
+import SwiftUI
+
+@main
+struct hundredDaysSwiftUI_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
