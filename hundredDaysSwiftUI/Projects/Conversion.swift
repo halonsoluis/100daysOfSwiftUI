0489d6484f7bf88de59493521a3a7ae9e8541d9b
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-06-19 22:28:36 +0200

    Simplify even more by using built in functionality, remove extra file as to have all in the same place

--- hundredDaysSwiftUI/Projects/Conversion.swift
+++ hundredDaysSwiftUI/Projects/Conversion.swift
@@ -3,14 +3,12 @@
 
 import SwiftUI
 
-enum UnitTemperature: String, CaseIterable {
-    case celsius, farenheit, kelvin
-}
-
 struct Conversion: View {
     @State private var originalUnit: UnitTemperature = .celsius
-    @State private var destinationUnit: UnitTemperature = .farenheit
+    @State private var destinationUnit: UnitTemperature = .fahrenheit
     @State private var value: Double = 0
+
+    private let units: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
 
     var body: some View {
         NavigationView {
@@ -18,16 +16,16 @@ struct Conversion: View {
 
                 Section("Select original unit") {
                     Picker("original unit", selection: $originalUnit) {
-                        ForEach(UnitTemperature.allCases, id: \.self) {
-                            Text("\($0.rawValue)")
+                        ForEach(units, id: \.self) {
+                            Text("\($0.symbol)")
                         }
                     }.pickerStyle(.segmented)
                 }
 
                 Section("Select destination unit") {
                     Picker("original unit", selection: $destinationUnit) {
-                        ForEach(UnitTemperature.allCases, id: \.self) {
-                            Text("\($0.rawValue)")
+                        ForEach(units, id: \.self) {
+                            Text("\($0.symbol)")
                         }
                     }.pickerStyle(.segmented)
                 }
@@ -46,8 +44,13 @@ struct Conversion: View {
         .navigationTitle("Temperature conversion")
     }
 
-    var convertedAmount: Double {
-        Converter.convert(value, from: originalUnit, to: destinationUnit)
+    private var convertedAmount: Double {
+        NSMeasurement(
+            doubleValue: value,
+            unit: originalUnit
+        )
+        .converting(to: destinationUnit)
+        .value
     }
 }
 
