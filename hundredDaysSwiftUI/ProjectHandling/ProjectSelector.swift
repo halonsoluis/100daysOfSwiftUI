59d845e02e80957146224d12bd1648d53c3bfaf5
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-06-20 10:55:02 +0200

    Add scaffolding for flags project

--- hundredDaysSwiftUI/ProjectHandling/ProjectSelector.swift
+++ hundredDaysSwiftUI/ProjectHandling/ProjectSelector.swift
@@ -8,8 +8,10 @@ struct ProjectSelector: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 10) {
+
                 NavigationLink("WeSplit", destination: WeSplit())
                 NavigationLink("Conversion", destination: Conversion())
+                NavigationLink("Flags", destination: Flags())
 
             }
             .navigationTitle("100 days of SwiftUI")
---
+++ hundredDaysSwiftUI/Projects/Flags.swift
@@ -0,0 +1,16 @@
+// Created for hundredDaysSwiftUI in 2023
+// Using Swift 5.0
+
+import SwiftUI
+
+struct Flags: View {
+    var body: some View {
+        Text("Select the flag! 🇪🇸")
+    }
+}
+
+struct Flags_Previews: PreviewProvider {
+    static var previews: some View {
+        Flags()
+    }
+}
