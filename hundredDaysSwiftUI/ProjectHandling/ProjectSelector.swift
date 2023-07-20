6472394f7dc7bd6f9ffda03e6cf5c5fcb287995d
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-07-20 23:46:29 +0200

    Add some gradient to the menu, it was looking awful

--- hundredDaysSwiftUI/ProjectHandling/ProjectSelector.swift
+++ hundredDaysSwiftUI/ProjectHandling/ProjectSelector.swift
@@ -6,18 +6,33 @@ import SwiftUI
 struct ProjectSelector: View {
 
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 10) {
+            NavigationStack {
 
-                NavigationLink("WeSplit", destination: WeSplit())
-                NavigationLink("Conversion", destination: Conversion())
-                NavigationLink("Flags", destination: Flags())
-                NavigationLink("RockPaperScissors", destination: RockPaperScissors())
-                NavigationLink("Scramble", destination: Scramble())
+                ZStack {
 
+                    RadialGradient(
+                        colors: [.black, .red, .black, .black, .black, .red, .black, .black],
+                        center: .center,
+                        startRadius: 280,
+                        endRadius: 900
+                    )
+                    .ignoresSafeArea()
+
+                VStack(alignment: .leading, spacing: 10) {
+
+                    NavigationLink("WeSplit", destination: WeSplit())
+                    NavigationLink("Conversion", destination: Conversion())
+                    NavigationLink("Flags", destination: Flags())
+                    NavigationLink("RockPaperScissors", destination: RockPaperScissors())
+                    NavigationLink("Scramble", destination: Scramble())
+
+                }
+                .padding(20)
+                .font(.title)
+                .foregroundColor(.white)
+                .navigationTitle("100 days of SwiftUI")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .navigationTitle("100 days of SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
