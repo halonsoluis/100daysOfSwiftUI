d7f5f7c8c5f6a699b834bfd115a0e1d8f9b173b0
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-07-26 16:30:18 +0200

    Add initial game UI

--- hundredDaysSwiftUI/Projects/Multiplicate/Multiplicate.swift
+++ hundredDaysSwiftUI/Projects/Multiplicate/Multiplicate.swift
@@ -3,9 +3,58 @@
 
 import SwiftUI
 
+
+
+///Appealing idea..
+///
+///Make  recordigns of previous games of the person, in terms of time per qeustion.
+///Then, in dependence of the difficulty provide a value to use to increase the empire.
+///For now, empire can be red vs blue. even more colors could apply.
+///
+///- remember the ones you faul often.
+
 struct Multiplicate: View {
+
+    @FocusState private var answerIsFocused: Bool
+    @State var leftOperand = 7
+    @State var rightOperand = 7
+    @State var answer: Int?
+    @State var correct = false
+
+    var isCorrectAnswer: Bool {
+        leftOperand * rightOperand == answer
+    }
+
     var body: some View {
-        Text("Multiplicate")
+
+        HStack(alignment: .center) {
+            Text(leftOperand, format: .number)
+            Text("x")
+            Text(rightOperand, format: .number)
+            Text(" = ")
+
+            TextField("__", value: $answer, format: .number)
+                .keyboardType(.numberPad)
+                .labelsHidden()
+                .focused($answerIsFocused)
+                .frame(width: 50)
+        }
+        .font(.largeTitle)
+        .foregroundColor(answer == nil
+                         ? .primary
+                         : isCorrectAnswer
+                            ? .green
+                            : .red
+        )
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+
+                Button("Done") {
+                    answerIsFocused = !isCorrectAnswer
+                }
+            }
+        }
     }
 }
 
