736733e2109cf5db3ae6599ab13104c39aae2eae
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-07-30 14:32:24 +0200

    Present and dismiss sheets

--- hundredDaysSwiftUI/Projects/iExpense/iExpense.swift
+++ hundredDaysSwiftUI/Projects/iExpense/iExpense.swift
@@ -10,13 +10,41 @@ class User: ObservableObject {
 
 struct iExpense: View {
     @StateObject private var user = User()
+    @State private var showingSheet = false
 
     var body: some View {
         VStack {
+            Spacer()
+
             Text("Your name is \(user.firstName) \(user.lastName).")
 
             TextField("First name", text: $user.firstName)
             TextField("Last name", text: $user.lastName)
+
+            Spacer()
+
+            Button("Show Sheet") {
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: user.firstName)
+            }
+
+            Spacer()
+        }
+    }
+}
+
+struct SecondView: View {
+    let name: String
+
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        Text("Hello, \(name)!")
+
+        Button("Dismiss") {
+            dismiss()
         }
     }
 }
