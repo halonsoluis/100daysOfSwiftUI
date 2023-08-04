4e65f939f2c19d82838fe20bdf4df3d3cdf110a4
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2023-08-04 19:28:00 +0200

    Add iExpense code for the list of expenses, with styling

--- hundredDaysSwiftUI/Projects/iExpense/iExpense.swift
+++ hundredDaysSwiftUI/Projects/iExpense/iExpense.swift
@@ -3,47 +3,109 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items: [ExpenseItem]
+
+    init(items: [ExpenseItem]) {
+        self.items = items
+    }
 }
 
 struct iExpense: View {
-    @StateObject private var user = User()
+    @StateObject private var expenses = Expenses(items: [
+        ExpenseItem(name: "example", type: "private", amount: 100),
+        ExpenseItem(name: "example 2", type: "private", amount: 1000),
+        ExpenseItem(name: "example 3", type: "public", amount: 8),
+    ])
     @State private var showingSheet = false
 
     var body: some View {
-        VStack {
-            Spacer()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(expenses.items) { expense in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(expense.name)
+                                    .font(.headline)
+                                    .textCase(.uppercase)
+                                Text(expense.type)
+                                    .font(.subheadline)
+                            }
+                            Spacer()
 
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-
-            Spacer()
-
-            Button("Show Sheet") {
-                showingSheet.toggle()
+                            Text(expense.amount,
+                                 format: .currency(
+                                    code: Locale.current.currency?.identifier ?? "EUR"
+                                 )
+                            )
+                            .font(.callout)
+                            .styled(amount: expense.amount)
+                        }
+                    }
+                }
             }
-            .sheet(isPresented: $showingSheet) {
-                SecondView(user: user)
+            .toolbar {
+                Button {
+                    showingSheet.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(expenses: expenses)
+                }
             }
-
-            Spacer()
+            .navigationTitle("iExpense")
         }
     }
 }
 
+struct StyledAmount: ViewModifier {
+    let under10 = Color.red
+    let under100 = Color.blue
+    let over100 = Color.green
+
+    let amount: Int
+
+    var style: Color {
+        guard amount > 10 else {
+            return under10
+        }
+        guard amount > 100 else {
+            return under100
+        }
+        return over100
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(style)
+    }
+}
+
+extension Text {
+    func styled(amount: Int) -> some View {
+        modifier(StyledAmount(amount: amount))
+    }
+}
+
 struct SecondView: View {
-    @ObservedObject var user: User
+    @ObservedObject var expenses: Expenses
 
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
-        Text("Hello, \(user.firstName)!")
 
         Button("Dismiss") {
+            expenses.items
+                .append(ExpenseItem(name: "new expense", type: "private", amount: 10))
             dismiss()
         }
     }
